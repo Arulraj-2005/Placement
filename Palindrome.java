8a3d5c8221230bc335ec0a1b6eb98e341244fe6c
Arulraj-2005 <arulrajrtc2005@gmail.com>
2026-04-28 09:30:57 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Palindrome {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int num=sc.nextInt();
+    int n=Math.abs(num);
+    int original=n;
+    int rev=0;
+    while(n>0){
+        int rem=n%10;
+        rev=rev*10+rem;
+        n=n/10;
+    }
+    if(original==rev){
+        System.out.println("Palindrome");
+    }
+    else{
+        System.out.println("Not a Palindrome");
+    }
+}
+}
